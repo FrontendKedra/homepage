cca37ba0b014ef07310791f93da3b664e00b333f
Szymon Kedra <s5zee@wp.pl>
2022-07-19 17:05:04 +0200

    split js code into functions for better visibility

--- js/script.js
+++ js/script.js
@@ -1,11 +1,24 @@
-let buttonElement = document.querySelector(".js-button");
-let body = document.querySelector(".js-body");
+{
+    const onChangeBackgroundClick = () => {
+        const body = document.querySelector(".js-body");
+        body.classList.toggle("body-dark");
+    }
 
-buttonElement.addEventListener("click", () => {
-    if(buttonElement.innerText === "Włącz ciemniejsze tło") {
-        buttonElement.innerText = "Wyłącz ciemniejsze tło";
-    }  else {
-        buttonElement.innerText = "Włącz ciemniejsze tło";
+    const toggleButtonText = () => {
+        const buttonElement = document.querySelector(".js-button");
+
+        if (buttonElement.innerText === "Włącz ciemniejsze tło") {
+            buttonElement.innerText = "Wyłącz ciemniejsze tło";
+        } else {
+            buttonElement.innerText = "Włącz ciemniejsze tło";
+        };
+    };
+
+    const init = () => {
+        const buttonElement = document.querySelector(".js-button");
+        buttonElement.addEventListener("click", onChangeBackgroundClick);
+        buttonElement.addEventListener("click", toggleButtonText);
+    };
+
+    init();
 }
-    body.classList.toggle("body-dark");
-})
